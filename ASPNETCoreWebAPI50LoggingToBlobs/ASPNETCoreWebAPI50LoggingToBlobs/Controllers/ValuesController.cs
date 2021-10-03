c7d9b7ff356b5c1d4169791e5870cb4b21cb8745
Henri Lo <henri.lo@iversis.com.au>
2021-10-03 23:13:18 +1100

    first check in

---
+++ ASPNETCoreWebAPI50LoggingToBlobs/ASPNETCoreWebAPI50LoggingToBlobs/Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPNETCoreWebAPI50LoggingToBlobs.Controllers
+{
+    [Route("api/values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private ILogger<ValuesController> logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            this.logger = logger;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            logger.LogInformation("Some information logged here.");
+
+            return new string[] { "value1", "value2" };
+        }
+        
+    }
+}
